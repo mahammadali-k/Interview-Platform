5a18493927eb23a9a5b86c41e2fe72045888322f
mahammadali-k <mkerimov892@gmail.com>
2025-11-07 14:21:11 +0400

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import { ENV } from "./lib/env.js"
+
+const app = express();
+
+console.log(ENV.PORT);
+console.log(ENV.DB_URL);
+
+
+app.get('/health', (req, res) => {
+  res.status(200).json ({ msg: 'Success from backend' });
+});
+
+
+app.listen(ENV.PORT, () => console.log('Server is running on port 3000'));
